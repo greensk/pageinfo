4c15a5526ae9a957e4b51a68aa6ad089acd58599
Grigory Kosheev <grigory@kosheev.com>
2017-12-02 23:22:18 +0700

    implement core

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# pageinfo
+#pageinfo
+
+This script fetches information about webpage by given url. It performs a simple try to determine website name and content article name. Otherwise it uses domain name like website name and page title like content article name. Supports only http and https.
+
+Usage:
+```
+http://pageinfo.mydomain.com/?url=http://tasks.z/pageinfo/?url=http://www.bbc.com/news/world-us-canada-42205181
+```
+Example output:
+```
+{"success":true,"soruce":"BBC News","name":"Tax bill: Trump victory as Senate backs tax overhaul"}
+```
+
+Security warning! Requests to localhost are not restricted. Please make sure you server does not output some private info for localhost via http. Note that user can switch port.
+
---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+if (empty($_GET['url'])) {
+	show_error('Please pass the url via "url" param');
+}
+$url = $_GET['url'];
+
+if (strpos('http://', $url) !== 0 && strpos('https://', $url)) {
+	show_error('The url should start with http:// or https://');
+}
+
+$title = website_title($url);
+
+if ($title === null) {
+	show_error('Cannot read the url');
+}
+
+if (substr_count($title, '&mdash;') === 1) {
+	$parts = explode('&mdash;', $title);
+	$article = $parts[0];
+	$source = $parts[1];
+} else if (substr_count($title, '/') === 1) {
+	$parts = explode('/', $title);
+	$article = $parts[0];
+	$source = $parts[1];
+}  else if (substr_count($title, '—') === 1) {
+	$parts = explode('—', $title);
+	$article = $parts[0];
+	$source = $parts[1];
+} else if (substr_count($title, '-') === 1) {
+	$parts = explode('-', $title);
+	$article = $parts[0];
+	$source = $parts[1];
+} else {
+	$article = $title;
+	
+	$source = parse_url($url, PHP_URL_HOST);
+}
+
+header('Content-Type: application/json; charset=UTF-8');
+echo json_encode(['success' => true, 'soruce' => trim($source), 'name' => trim($article)], JSON_UNESCAPED_UNICODE);
+
+function website_title($url) {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$html = curl_exec($ch);
+	curl_close($ch);
+
+	$dom  = new DOMDocument;
+	@$dom->loadHTML($html);
+
+	if ($dom) {
+		$title = @$dom->getElementsByTagName('title')->item('0')->nodeValue;
+		return $title;
+	} else {
+		return null;
+	}
+}
+
+function show_error($message) {
+	die(json_encode(['success' => false, 'message' => $message]));
+}
